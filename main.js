6c74df80b5332a3c3dcd35af4575e7fa4bbace85
ISHMA KHAN <149327188+ishma2754@users.noreply.github.com>
2024-03-14 14:16:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,117 @@
+let shop = document.getElementById('shop');
+
+let shopItemsData = [
+  {
+    id: 'jjjj',
+    name: 'Power Rack',
+    price: 100,
+    desc: 'Mechanical spotter for free weight barbell exercises',
+    img: 'images/img-1.jpg'
+  }, 
+  {
+    id: 'kkkk',
+    name: 'Adjustable Dumbbell',
+    price: 40,
+    desc: 'Allows customizable weight training ',
+    img: 'images/img-2.jpg'
+  }, 
+  {
+    id: 'llll',
+    name: 'Yoga Mat',
+    price: 25,
+    desc: 'Provides comfort during mindful practice ',
+    img: 'images/img-3.jpg'
+  }, 
+  {
+    id: 'mmmm',
+    name: 'Kettlebell',
+    price: 30,
+    desc: 'Swings strength and versatility into workouts ',
+    img: 'images/img-4.jpg'
+
+}];
+
+
+let basket = [];
+
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let {id, name, price, desc, img} = x; //destructuring
+    return ` 
+    <div id=product-id-${id} class="item">
+      <img width="220" src="${img}" alt="">
+      <div class="details">
+        <h3>${name}</h3>
+        <p>${desc}</p>
+        <div class="price-quantity">
+          <h2>$ ${price}</h2>
+          <div class="buttons">
+            <i onclick="decrement(${id});" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">0</div>
+            <i onclick="increment(${id});" class="bi bi-plus-lg"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  }).join(''));
+};
+
+generateShop();
+
+//create search function find if the item exist if it does then increse quantity or not then push the item
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);//If a match is found, the search variable will hold that element (an object).
+  //If no match is found, search will be undefined.
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1
+    });
+  
+  } else {
+    search.item += 1;
+  }
+ 
+
+  update(selectedItem.id);
+  
+}
+
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);//If a match is found, the search variable will hold that element (an object).
+  //If no match is found, search will be undefined.
+
+  //If the current quantity (search.item) is equal to zero, it means the item has already been decremented to its minimum value.
+  
+  if (search.item === 0) return;
+   else {
+    search.item -= 1;
+  }
+ 
+ 
+  update(selectedItem.id);
+  
+}
+
+
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById('cartAmount');
+  //return only item not id
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  
+};
---
+++ new.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 'jssss',
+    name: 'Casual Shirt',
+    price: 45,
+    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    img: 'images/img-1.jpg'
+  }, 
+  {
+    id: 'kkkk',
+    name: 'Office Shirt',
+    price: 100,
+    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    img: 'images/img-2.jpg'
+  }, 
+  {
+    id: 'llll',
+    name: 'T Shirt',
+    price: 25,
+    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    img: 'images/img-3.jpg'
+  }, 
+  {
+    id: 'mmmm',
+    name: 'Mens Suit ',
+    price: 300,
+    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    img: 'images/img-4.jpg'
+
+}]
+
+

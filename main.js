aaf704a5d996762b6a4710e81cece143ec23a9c9
ishma2754 <ishmakhan1995@gmail.com>
2024-03-15 16:49:58 +0530

    change the color theme

--- main.js
+++ main.js
@@ -29,6 +29,32 @@ let shopItemsData = [
     desc: 'Swings strength and versatility into workouts ',
     img: 'images/img-4.jpg'
 
+}, {
+  
+    id: 'nnnn',
+    name: 'Barbell',
+    price: 60,
+    desc: ' Provides resistance to help you build muscle ',
+    img: 'images/img-5.png'
+
+}, {
+    id: 'oooo',
+    name: 'Pull up bar',
+    price: 60,
+    desc: 'Rise, conquer: Pull-up bar, strength ascends',
+    img: 'images/img-6.png'
+}, {
+    id: 'pppp',
+    name: 'Bench',
+    price: 80,
+    desc: 'Strength rests here: Bench, sculpting power and form.',
+    img: 'images/img-7.jpg'
+}, {
+    id: 'oooo',
+    name: 'Weight Plates',
+    price: 30,
+    desc: 'symphony: Gym plates, sculpting strength in silence',
+    img: 'images/img-8.jpg'
 }];
 
 
--- new.js
+++ new.js
@@ -1,32 +1,73 @@
-const products = [
+let shop = document.getElementById('shop');
+
+let shopItemsData = [
   {
-    id: 'jssss',
-    name: 'Casual Shirt',
-    price: 45,
-    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    id: 'jjjj',
+    name: 'Power Rack',
+    price: 100,
+    desc: 'Mechanical spotter for free weight barbell exercises',
     img: 'images/img-1.jpg'
   }, 
   {
     id: 'kkkk',
-    name: 'Office Shirt',
-    price: 100,
-    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    name: 'Adjustable Dumbbell',
+    price: 40,
+    desc: 'Allows customizable weight training ',
     img: 'images/img-2.jpg'
   }, 
   {
     id: 'llll',
-    name: 'T Shirt',
+    name: 'Yoga Mat',
     price: 25,
-    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    desc: 'Provides comfort during mindful practice ',
     img: 'images/img-3.jpg'
   }, 
   {
     id: 'mmmm',
-    name: 'Mens Suit ',
-    price: 300,
-    desc: ' Lorem ipsum, dolor sit amet consectetur adipisicing.',
+    name: 'Kettlebell',
+    price: 30,
+    desc: 'Swings strength and versatility into workouts ',
     img: 'images/img-4.jpg'
 
-}]
+}, {
+    id: 'mmmm',
+    name: 'Kettlebell',
+    price: 30,
+    desc: 'Swings strength and versatility into workouts ',
+    img: 'images/img-4.jpg'
+
+}];
 
 
+let basket = [];
+const jtml = require('jtml');
+
+
+
+
+const generateShop = () => {
+  return jtml.html`
+    ${shopItemsData.map((x) => {
+      const { id, name, price, desc, img } = x; 
+      return html `
+        <div id="product-id-${id}" class="item">
+          <img width="220" src="${img}" alt="">
+          <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
+            <div class="price-quantity">
+              <h2>$ ${price}</h2>
+              <div class="buttons">
+                <i onclick="decrement(${id});" class="bi bi-dash-lg"></i>
+                <div id="${id}" class="quantity">0</div>
+                <i onclick="increment(${id});" class="bi bi-plus-lg"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    }).join('')}
+  `;
+};
+
+shop.innerHTML = generateShop();
